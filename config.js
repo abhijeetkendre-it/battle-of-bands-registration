2972c9899256939646cdb1f8d5e7bf7dfaeee6a9
Abhijeet Kendre <theabhijeetkendre@gmail.com>
2026-06-22 00:55:24 +0530

    chore: update to new Google Sheet deployment URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Application Configuration for GitHub Pages Deployment
 const CONFIG = {
   // Google Sheets Apps Script Web App URL
-  GOOGLE_SHEET_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbxsW3eTo6YClhbsUcyT_WbnIObk0C9XyS9qy8RHJoc5WBYpJBK3ynT90-F4M0wf3CnE/exec",
+  GOOGLE_SHEET_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbxpuTj9BStxvF1U95nmg-dxPWNHz6KQzWYdhHOyvQ-o2woCXt_XegI-IlfvqTPlRCqAoQ/exec",
   
   // Official Event WhatsApp Group Link
   WHATSAPP_LINK: "https://chat.whatsapp.com/BTyHVmOoSBrKuplPGNDimx"
